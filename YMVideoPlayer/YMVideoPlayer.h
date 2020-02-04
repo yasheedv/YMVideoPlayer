f853aa4f952d42ab7101aa6fdf40caea1340104e
Yasheed Muhammed <yasheedv@gmail.com>
2020-02-04 14:21:12 +0400

    Initial Commit

---
+++ YMVideoPlayer/YMVideoPlayer.h
@@ -0,0 +1,19 @@
+//
+//  YMVideoPlayer.h
+//  YMVideoPlayer
+//
+//  Created by Yasheed Muhammed on 04/02/2020.
+//  Copyright © 2020 yasheed. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for YMVideoPlayer.
+FOUNDATION_EXPORT double YMVideoPlayerVersionNumber;
+
+//! Project version string for YMVideoPlayer.
+FOUNDATION_EXPORT const unsigned char YMVideoPlayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YMVideoPlayer/PublicHeader.h>
+
+
